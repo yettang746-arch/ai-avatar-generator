41b09fae937365e32a123bd0b60f3dcb147460b2
ettang746 <ettang746@gmail.com>
2026-02-04 22:20:35 +0800

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H6J5MBJ7R5"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);} 
+gtag('js', new Date());
+
+gtag('config', 'G-H6J5MBJ7R5');`
+          }}
+        />
+      </head>
       <body className="antialiased">
         {children}
       </body>
